65d2d60327e2ec46f62464fecb49e49e9ee0a63a
MingYuanTina <bjteana@gmail.com>
2024-01-26 11:14:03 -0500

    update logo

--- antifragile/docusaurus.config.js
+++ antifragile/docusaurus.config.js
@@ -77,12 +77,12 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/docusaurus.png',
       navbar: {
         title: 'Capy',
         logo: {
           alt: 'Capy Logo',
-          src: 'img/logo.svg',
+          src: 'img/docusaurus.png',
         },
         items: [
           {
